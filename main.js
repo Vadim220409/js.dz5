6a50a7cb1a491b20cd38ed7809cbcdd17a4c9b20
Vadim220409 <107919314+Vadim220409@users.noreply.github.com>
2023-03-09 18:22:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// ex 1
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(numbers.length);
+
+// ex 2
+
+let names = ["Andrew", "Maksim", "Oleg", "Vadim", "Artem", "Ivan", "Bogdan", "Alex", "Stas", "Nikita",];
+console.log(names[0]);
+console.log(names[3]);
+console.log(names[7]);
+
+// ex 3
+
+let data = []; 
+console.log(data);
+data[0] = 10;
+data[1] = 8;
+data[4] = 100;
+console.log(data);
